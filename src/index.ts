485326a79b6ca19eea48ff04113fe164eacacfe8
OmarSaleh506 <amory2000338@gmail.com>
2022-06-26 12:00:57 +0300

    edite

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { server } from './server';
 
-server.listen({ port: 4500 }).catch((err) => {
-	server.log.error(err);
-	process.exit(1);
-});
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
+
+server
+	.listen({
+		port: port,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	});
